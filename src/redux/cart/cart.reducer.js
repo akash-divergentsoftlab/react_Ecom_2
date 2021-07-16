ded0ccc3a36c7a6e05cc8dc5babd74fa828054c6
akash-divergentsoftlab <akash.poddar@divergentsl.com>
2021-07-16 17:12:05 +0530

    added fully functional checkout page

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -8,23 +8,38 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+
+        // it hides the cart automatically when not needed
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
             }
+
+        // it adds and item to the cart
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
             };
+
+        // it removes an item from the cart
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return {
                 ...state,
                 cartItems: state.cartItems.filter(
                     cartItem => cartItem.id !== action.payload.id
                 )
+
             }
+
+        // it decreases an item from the checkout page 
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+
         default:
             return state;
     }
