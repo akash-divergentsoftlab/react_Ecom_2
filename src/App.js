969fd911087d6bdcaaf63736a4cbca9c379d736e
akash-divergentsoftlab <akash.poddar@divergentsl.com>
2021-07-16 12:36:22 +0530

    added checkout page with routing and added minimum features that renders total amount and all producst page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in and sign-up/sign-in and sign-up.component'
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import React from 'react';
+import './App.css';
+
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
+import { createStructuredSelector } from 'reselect';
+import { selectCurrentUser } from './redux/user/user.selector';
+
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in and sign-up/sign-in and sign-up.component'
+import ShopPage from './pages/shop/shop.component';
+import HomePage from './pages/homepage/homepage.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+
+
+
+
+
 
 class App extends React.Component {
   unsubscribeFromAuth = null
@@ -42,6 +55,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin'
             render={() =>
               this.props.currentUser ? (<Redirect to='/' />
@@ -56,8 +70,8 @@ class App extends React.Component {
     );
   }
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
